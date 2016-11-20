20a2f7d82ccb63a967616415adc2c1161b09f766
Philippe Bloom <philippe.bloom@produweb.be>
2016-11-20 12:07:09 +0100

    rearrangement

--- page.php
+++ page.php
@@ -1,38 +1,33 @@
 <?php
 /**
- * The template for displaying pages
+ * Template Name: contact
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage DecobootsTheme
- * @since Decoboots Theme 1.0.0
  */
 
-get_header(); ?>
+get_header();
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+?>
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
-
-			// Include the page content template.
-			get_template_part( 'content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		// End the loop.
-		endwhile;
-		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
+<?php while (have_posts()) :
+	the_post();
+	?>
+	<?php
+	include(locate_template('content/content-breadcrumb.php'));
+	?>
+	<div class="row row-content-page">
+		<div class="container">
+			<h3>
+				<?php
+				echo  get_the_title();
+				?>
+			</h3>
+			<p class="intro">
+				<?php
+				echo get_the_content();
+				?>
+			</p>
+		</div>
+	</div>
+	<?php
+endwhile; ?>
 <?php get_footer(); ?>
